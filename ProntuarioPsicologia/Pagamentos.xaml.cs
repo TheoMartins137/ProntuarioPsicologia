1e1a523953dc42d24263c40129a1d624be4d3d13
Theodoro Martins <theomartins137@gmail.com>
2025-08-02 22:33:08 -0300

    feat: Valor total pago pelo paciente adicionado

--- ProntuarioPsicologia/Pagamentos.xaml.cs
+++ ProntuarioPsicologia/Pagamentos.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,103 +25,28 @@ namespace ProntuarioPsicologia
         public Pagamentos()
         {
             InitializeComponent();
+            DataPagamento.SelectedDate = DateTime.Now;
         }
 
         public MySqlConnection Conexao = new MySqlConnection();
         private string data_source = "datasource=localhost;username=root;password=Martinsfreitas8;database=db_prontuario";
 
-        DateTime data = DateTime.Today;
+        DateTime DataSelecionada;
 
         private void Pagamentos1_Loaded(object sender, RoutedEventArgs e)
         {
             LstPagamentos.Items.Clear();
+            PesquisaData();
+        }
 
-            foreach (ListaPacientes pacientes in ListaPacientes.lista)
-            {
-                using (var conexao = new MySqlConnection(data_source))
-                {
-                    conexao.Open();
-
-                    string sql = "SELECT id_pagamento, valor, data_pagamento FROM pagamentos WHERE id_pacientes = @id AND MONTH(data_pagamento) = @mes AND YEAR(data_pagamento) = @ano ORDER BY id_pagamento ASC";
-                    using (MySqlCommand comand = new MySqlCommand(sql, conexao))
-                    {
-                        comand.Parameters.AddWithValue("@id", pacientes.id);
-                        comand.Parameters.AddWithValue("@mes", data.Month);
-                        comand.Parameters.AddWithValue("@ano", data.Year);
-
-                        using (MySqlDataReader reader = comand.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                var pagamento = new HistoricoPago
-                                {
-                                    id_pagamento = reader.GetInt32(0),
-                                    valor = reader.GetFloat(1),
-                                    data_pagamento = reader.GetDateTime(2),
-                                };
-                                LstPagamentos.Items.Add(pagamento);
-                            }
-                        }
-                    }
-                }
-            }
+        private void DataPagamento_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LstPagamentos.Items.Clear();
+            PesquisaData();
         }
 
         private void PesquisaData()
         {
-
-            LstPagamentos.Items.Clear();
-
-            foreach (ListaPacientes pacientes in ListaPacientes.lista)
-            {
-                using (var conexao = new MySqlConnection(data_source))
-                {
-                    conexao.Open();
-
-                    string sql = "SELECT id_pagamento, valor, data_pagamento FROM pagamentos WHERE id_pacientes = @id AND MONTH(data_pagamento) = @mes AND YEAR(data_pagamento) = @ano ORDER BY id_pagamento ASC";
-                    using (MySqlCommand comand = new MySqlCommand(sql, conexao))
-                    {
-                        comand.Parameters.AddWithValue("@id", pacientes.id);
-                        comand.Parameters.AddWithValue("@mes", data.Month);
-                        comand.Parameters.AddWithValue("@ano", data.Year);
-
-                        using (MySqlDataReader reader = comand.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                var pagamento = new HistoricoPago
-                                {
-                                    id_pagamento = reader.GetInt32(0),
-                                    valor = reader.GetFloat(1),
-                                    data_pagamento = reader.GetDateTime(2),
-                                };
-                                LstPagamentos.Items.Add(pagamento);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        private void btnVoltar_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnAdicionar_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-            AdicionarPagamento adicionarPagamento = new AdicionarPagamento();
-            adicionarPagamento.ShowDialog();
-            this.Show();
-            PesquisaData();
-        }
-
-        DateTime DataSelecionada;
-
-        private void DataPagamento_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        {
-
             DataSelecionada = Convert.ToDateTime(DataPagamento.Text);
             LstPagamentos.Items.Clear();
 
@@ -151,8 +77,42 @@ namespace ProntuarioPsicologia
                             }
                         }
                     }
+
+                    txtTotal.Text = "";
+
+                    string sql2 = "SELECT SUM(valor) FROM pagamentos WHERE id_pacientes = @id AND MONTH(data_pagamento) = @mes AND YEAR(data_pagamento) = @ano";
+                    using (MySqlCommand comand2 = new MySqlCommand(sql2, conexao))
+                    {
+                        comand2.Parameters.AddWithValue("@id", pacientes.id);
+                        comand2.Parameters.AddWithValue("@mes", DataSelecionada.Month);
+                        comand2.Parameters.AddWithValue("@ano", DataSelecionada.Year);
+
+                        using (MySqlDataReader reader2 = comand2.ExecuteReader())
+                        {
+                            while (reader2.Read())
+                            {
+                               txtTotal.Text = Convert.ToString(reader2.GetDecimal(0));
+                            }
+                        }
+                    }
+                        
                 }
             }
+        }
+
+
+        private void btnVoltar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            AdicionarPagamento adicionarPagamento = new AdicionarPagamento();
+            adicionarPagamento.ShowDialog();
+            this.Show();
+            PesquisaData();
         }
 
         private void DataPagamento_CalendarOpened(object sender, RoutedEventArgs e)
